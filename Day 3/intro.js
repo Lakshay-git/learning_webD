440111305a6cc9aaa3e80f6bb0dbe095029c84ed
Lakshay soni <lakshaysoni5252@gmail.com>
2024-01-17 22:30:45 +0530

    ..

---
+++ Day 3/intro.js
@@ -0,0 +1,9 @@
+// let person = { firstName: "john", lastName:f"Doe",age: " 19"}
+num1 = 5
+num2 = 2
+if(num2>num1){
+    console.log(num2+"is greater than "+ num1);
+}else{
+    console.log(num2+"is not greater than "+ num1)
+}
+//terary operator 
