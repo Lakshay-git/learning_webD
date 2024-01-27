06a8fdb7992529222758913ecdff03c3747a92cb
Lakshay soni <lakshaysoni5252@gmail.com>
2024-01-27 22:41:43 +0530

    ..

--- DAY 7/scope.js
+++ DAY 7/scope.js
@@ -1,19 +1,19 @@
-// Global letiable
+
 let global_letiable = 20;
  
 function main_function() {
-    // Local letiable
+
     let local_letiable = 30;
  
     let nested_function = function () {
  
-        // Display the value inside the local letiable
+
         console.log(local_letiable);
     }
  
     let another_nested_function = function () {
  
-        // Displays the value inside the global letiable
+
         console.log(global_letiable);
     }
  
