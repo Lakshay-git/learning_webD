57803ba5d69d8986283f23b4d165cae226724d33
Lakshay soni <lakshaysoni5252@gmail.com>
2024-01-29 23:41:32 +0530

    UPDATE

--- Day 2/portfolio copy/menu.js
+++ Day 2/portfolio copy/menu.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded",function(){
-    document.getElementById("menu-button").addEventListener("click , showMenu")
-    document.getElementById("close-menu").addEventListener("click" , this.close)
-    function showMenu(){
-        const menu = document.querySelector("section.menu");
-        menu.style.height = "100%";
-
-    }
-    function closeMenu(){
-        const menu = document.querySelector("section.menu");
-        menu.style.height = "0";
-
-    }
+    document.getElementById("menu-button").addEventListener("click" , showMenu)
+    document.getElementById("menu-button-ka-lund").addEventListener("click" , lundMenu)
+    document.getElementById("close-menu").addEventListener("click" , closeMenu)
 
 })
+
+function lundMenu() {
+    const menu = document.querySelector("section.menu");
+    const lund = document.querySelector("#menu-button-ka-lund");
+    menu.style.display = "block";
+    lund.style.display = "none";
+}
+
+function showMenu(){
+    const menu = document.querySelector("section.menu");
+    menu.style.height = "100%";
+
+}
+
+function closeMenu(){
+    const menu = document.querySelector("section.menu");
+    const lund = document.querySelector("#menu-button-ka-lund");
+    menu.style.display = "none";
+    lund.style.display = "block";
+}
