58236875890ef3df55e52136d71f0178f5fddcee
Lakshay soni <lakshaysoni5252@gmail.com>
2024-02-05 23:21:22 +0530

    objects in java script

--- DAY 11/functionObjects.js
+++ DAY 11/functionObjects.js
@@ -1,4 +1,4 @@
-function printText(){
+function printText() {
     console.log("hello");
 }
 
@@ -7,9 +7,9 @@ printText.areYouAnObject = true;
 let a = 2;
 
 let person = {
-    firstName:"Lakshay",
-    lastName:"Soni",
-    getName : function(){
+    firstName: "Lakshay",
+    lastName: "Soni",
+    getName: function () {
         console.log(this);
         return this.firstName + " " + this.lastName
     }
@@ -19,16 +19,63 @@ let person = {
 console.log(this.a);
 console.log(person.getName());
 
-function greetPerson(){
-    console.log("Namaste",this.getName)
+function greetPerson(greeting) {
+    console.log(greeting, this.getName)
 }
 
-greetPerson(){
-    onslotchange.log("Namaste",this.getName());
-}
+// greetPerson(){
+//     onslotchange.log("Namaste",this.getName());
+// }
 
 greetPerson();
 
 // blind
+//create a new copy of the function that we calling bind
+// let greetLakshay = greetPerson.bind(person)
+// console.log(greetLakshay());
 
-greetPerson.bind()
+//call
+//doesn't create a copy 
+
+greetPerson.call(person, "Namaste")
+
+// apply 
+
+// arguments
+
+function sum() {
+    let total = 0;
+    // let total = x+y+z+f;
+
+    for (let index = 0; index < arguments.length; index++) {
+        total += arguments[index];
+    }
+    
+    return total;
+}
+
+console.log(sum(2, 3, 4, 5, 5))
+
+
+//apply
+
+greetPerson.apply(person, ["hello"]);
+
+//function borrowing
+
+let doctor = {
+    firstName : "manthan",
+    lastName : "soni"
+
+}
+console.log(person.getName.call(doctor))
+
+//function currying
+ 
+function logData(date, data){
+    console.log(date, data);
+}
+
+logData(new Date(), "this data to be logged")
+
+let logNow = logData.bind
