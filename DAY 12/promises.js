767fb94827a67021104f5e6bdbd73de6b92dfdd2
Lakshay soni <lakshaysoni5252@gmail.com>
2024-02-12 23:43:28 +0530

    promises

--- DAY 12/promises.js
+++ DAY 12/promises.js
@@ -1,16 +1,30 @@
+let moneyLended = 10_000;
 let promise = new Promise((resolve, reject) => {
+    console.log("in first prmosie");
     setTimeout(() => {
-        //resolve(10_000); //kept my promise
-        reject(new Error("i lied, you'r not getting your money back "))
+        console.log("resolving...")
+         //kept my promise
+        resolve(10_000);
+        // resolve(16_000);
+        // resolve(8_000);
+        //reject(new Error("i lied, you'r not getting your money back "))
     }, 3000)//represent two days
 });
 
 promise
     .then((money) => {
-        if (money === 10_000) {
+        console.log("in first then");
+        console.log(money);
+        if (money >= moneyLended) {
             console.log("Yay!, I got my money back ");
         }
-
+        return money;
+    }).then(moneyReceived).
+    then(extraMoney => {
+        if (extraMoney) {
+            console.log("wow my friend is generous, now i can buy some cars")
+        }
+        console.log("deposite the lended amount back to the back");
 
     }).catch(error => {
         console.error("☹️", error.message)
@@ -21,4 +35,20 @@ promise
 // rejected state - error
 // pending state - neither fullfilled nor rejected 
 // setteled state - it represents -fulfilled state / rejected state 
-// comment
+
+
+function moneyReceived(amountRecieved) {
+    return new Promise((resolve, reject) => {
+        console.log("in money received");
+        console.log(amountRecieved);
+        if (amountRecieved > moneyLended) {
+            resolve(amountRecieved - moneyLended);
+
+        } else if (amountRecieved < moneyLended) {
+            reject(new Error("oh wait, i have recieved the entire amount "))
+        } else {
+            console.log("resolving....")
+            resolve(0);
+        }
+    })
+}
