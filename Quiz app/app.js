9e9f538475869f25994c7ce6dcc7844d321c05bc
Lakshay soni <lakshaysoni5252@gmail.com>
2024-02-20 23:39:56 +0530

    Quiz App processing

---
+++ Quiz app/app.js
@@ -0,0 +1,27 @@
+const question = [{
+    'que' : "Which of the following is a markup language?",
+
+    'a' : 'HTML ',
+    'b' : 'javascript',
+    'c' : 'CSS',
+    'd' : 'PHP',
+    'Correct' : 'a'
+},
+{
+    'que' :" Which of the following is a markup language?",
+
+    'a' : 'HTML ',
+    'b' : 'javascript',
+    'c' : 'CSS',
+    'd' : 'PHP',
+    'Correct' : 'a'
+},
+{
+    'que' : "Which of the following is a markup language?",
+
+    'a' : 'HTML ',
+    'b' : 'javascript',
+    'c' : 'CSS',
+    'd' : 'PHP',
+    'Correct' : 'a'
+}]
