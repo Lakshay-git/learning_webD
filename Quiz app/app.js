24a8e63a6b7f45293c65f68498543af9405dfa88
Lakshay soni <lakshaysoni5252@gmail.com>
2024-02-21 23:32:43 +0530

    Quiz update

--- Quiz app/app.js
+++ Quiz app/app.js
@@ -1,27 +1,83 @@
 const question = [{
-    'que' : "Which of the following is a markup language?",
+    'que': "Which of the following is a markup language?",
 
-    'a' : 'HTML ',
-    'b' : 'javascript',
-    'c' : 'CSS',
-    'd' : 'PHP',
-    'Correct' : 'a'
+    'a': 'HTML ',
+    'b': 'javascript',
+    'c': 'CSS',
+    'd': 'PHP',
+    'Correct': 'a'
 },
 {
-    'que' :" Which of the following is a markup language?",
+    'que': " What country has won the most World Cups? ",
 
-    'a' : 'HTML ',
-    'b' : 'javascript',
-    'c' : 'CSS',
-    'd' : 'PHP',
-    'Correct' : 'a'
+    'a': 'india ',
+    'b': 'brazil',
+    'c': 'australia',
+    'd': 'indonesia',
+    'Correct': 'b'
 },
 {
-    'que' : "Which of the following is a markup language?",
+    'que': "How many bones do we have in an ear?",
 
-    'a' : 'HTML ',
-    'b' : 'javascript',
-    'c' : 'CSS',
-    'd' : 'PHP',
-    'Correct' : 'a'
+    'a': '1 ',
+    'b': '9',
+    'c': '8',
+    'd': '3',
+    'Correct': 'd'
 }]
+
+let index = 0;
+let total = question.length;
+let right = 0;
+wrong = 0;
+const optionInputs = document.querySelectorAll('.options')
+const quesBox = document.getElementById("quesBox")
+const loadQuestion = () => {
+    reset();
+    const data = question[index]
+    
+    console.log(data)
+    quesBox.innerText = `${index + 1}) ${data.que}`;
+    optionInputs[0].nextElementSibling.innerText = data.a;
+    optionInputs[1].nextElementSibling.innerText = data.b;
+    optionInputs[2].nextElementSibling.innerText = data.c;
+    optionInputs[3].nextElementSibling.innerText = data.d;
+
+}
+
+
+const submitQuiz = () => {
+    const data = question[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        right++;
+    } else {
+        wrong++;
+    }
+    index++
+    loadQuestion();
+    return;
+}
+
+const getAnswer = () => {
+    optionInputs.forEach(
+        (input) => {
+            if (input.checked) {
+                return input.value;
+            }
+        }
+    )
+
+}
+
+const reset = () => {
+    optionInputs.forEach(
+        (input) => {
+            input.checked = false;
+        }
+    )
+}
+
+
+//initial call 
+loadQuestion()
